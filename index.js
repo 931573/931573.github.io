90f044a4712ff7d0678f3fbb5bc644936c20d19e
Kilian <ucwhx@student.kit.edu>
2024-01-09 16:53:15 +0100

    only mobile view now

--- index.js
+++ index.js
@@ -11,18 +11,18 @@ class tutorial{
     }
 }
 
-
 const addButton = document.querySelector('#add');
 const resetButton = document.querySelector("#reset");
 const startRankingButton = document.querySelector("#start_contest");
 const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
+// loads stored tutorials
 var storedTutorials = Object.keys(sessionStorage);
 for (let i = 0; i < storedTutorials.length; i++) {
     addtoTable(JSON.parse(sessionStorage.getItem(storedTutorials[i])));
 }
 
-
+// converts data to tutorial and adds it in storage
 addButton.onclick = () => {
     const name = document.querySelector('#name');
     const room = document.querySelector('#room');
@@ -32,11 +32,13 @@ addButton.onclick = () => {
 
     var newTutorial = new tutorial(name.value, room.value, time.value, weekday[asDate.getDay()]);
 
+    // everything has to be filled out 
     if (newTutorial.name === "" || newTutorial.room === "" || newTutorial.time === "" || newTutorial.day === "") {
         alert("Please fill out all inputs");
         return;
     }
 
+    // this tutorial was already added
     if (sessionStorage.getItem(JSON.stringify(newTutorial)) != null) {
         alert("Tutorial already added");
         return;
@@ -44,18 +46,19 @@ addButton.onclick = () => {
     var tutorialDiv = createTutorialDiv(newTutorial);
     sessionStorage.setItem(JSON.stringify(newTutorial), JSON.stringify(newTutorial));
     document.getElementsByClassName("tutorialTable")[0].appendChild(tutorialDiv);
+    // reset values
     name.value = "";    
     room.value = "";
     time.value = "";
     date.value = "";
 }
-
+// reset storage
 reset.onclick = () => {
     sessionStorage.clear();
     location.reload(true);
 }
 
-
+// navigates to new site if at least 3 tutorials have been added
 startRankingButton.onclick = () => {
     if (Object.keys(sessionStorage).length <= 2) {
         alert("Please enter at least 3 tutorials.");
@@ -64,38 +67,7 @@ startRankingButton.onclick = () => {
     document.location='swiss.html';
 }
 
-function createTutorialDiv(tutorial) {
-    var tutorialDiv = document.createElement("div");
-    tutorialDiv.setAttribute("class","tutorial");
-
-    var tempElement = document.createElement("h1");
-    tempElement.setAttribute("class", "title");
-    tempElement.appendChild(document.createTextNode(tutorial.title));
-    tutorialDiv.appendChild(tempElement);
-
-    tempElement = document.createElement("p");
-    tempElement.setAttribute("class", "stars inner");
-    if (tutorial.stars != 0) {
-        tempElement.append(document.createTextNode(tutorial.stars));
-
-    }
-    tutorialDiv.appendChild(tempElement);
-
-
-    tempElement = document.createElement("p");
-    tempElement.setAttribute("class", "day inner");
-    tempElement.appendChild(document.createTextNode(tutorial.day + " " + tutorial.time));
-    tutorialDiv.appendChild(tempElement);
-
-
-    tempElement = document.createElement("p");
-    tempElement.setAttribute("class", "room inner");
-    tempElement.appendChild(document.createTextNode(tutorial.room));
-    tutorialDiv.appendChild(tempElement);
-
-    return tutorialDiv;
-}
-
+// adds tutorial to the sites tutorialTable
 function addtoTable(tutDiv) {
     document.getElementsByClassName("tutorialTable")[0].appendChild(createTutorialDiv(tutDiv));
 }
